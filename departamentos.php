26f398a00eea09b6937ff97b63da2965dba6216e
unknown <marferisc@gmail.com>
2019-08-17 12:56:38 -0500

    punto de partida

---
+++ departamentos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ejercicio Departamentos</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<br><br>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 offset-md-3"> 
+				<h1 class="text-center bg-info">Departamentos</h1>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-8 offset-md-3">
+				<form action="" class="form-horizontal">
+						<div class="col-md-2">
+							<label for="" class="label-contol">Codigo</label>
+						</div>
+						<div class="col-md-5">
+							<input name="iddep" type="text" class="form-control" >
+						</div>
+						<div class="col-md-2">
+							<label for="" class="label-contol">Nombre</label>
+						</div>
+						<div class="col-md-5">
+							<input name="nomdep" type="text" class="form-control">
+						</div>
+						<div class="col-md-2">
+							<label for="" class="label-contol">Capital</label>
+						</div>
+						<div class="col-md-5">
+							<input name="capidep" type="text" class="form-control">
+						</div>
+						<div class="col-md-2">
+							<label for="" class="label-contol">N° Municipios</label>
+						</div>
+						<div class="col-md-5">
+							<input name="nummunicipios" type="text" class="form-control">
+						</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ejercicio Departamentos</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<br><br>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 offset-md-3"> 
+				<img src="img/colombia.jpg" alt="" class="img-fluid">
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-4 offset-md-5">
+				<a href="departamentos.php" class="btn btn-info">DEPARTAMENTOS</a>
+				<a href="" class="btn btn-info">MUNICIPIOS</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
